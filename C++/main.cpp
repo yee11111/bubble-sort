be8613a28d7040b93b584f7034f848eeea1a460c
yee11111 <44661756+yee11111@users.noreply.github.com>
2018-11-07 18:57:26 +0800

    Add files via upload

---
+++ C++/main.cpp
@@ -0,0 +1,43 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int bubblesort(int *ptr,int size);
+
+int main() {
+	int count;
+	int *ans;
+	scanf("%d",&count);
+	ans = new int[count];
+	for (int i = 0;i < count;i++) {
+		int size, *ptr;
+		scanf("%d",&size);
+		ptr = new int[size];
+		for (int j = 0;j < size;j++) {
+			scanf("%d", (ptr + j));
+		}
+		int a = bubblesort(ptr, size);
+		ans[i] = a;
+	}
+
+	for (int i = 0;i < count;i++) {
+		printf("Optimal train swapping takes %d swaps.\n", ans[i]);
+	}
+
+	system("pause");
+	return 0;
+}
+
+int bubblesort(int *ptr,int size) {
+	int chtime = 0;
+	for (int i = 0;i < size - 1;i++) {
+		for (int j = 0;j < size - 1 - i;j++) {
+			if (ptr[j] > ptr[j + 1]) {
+				int ch = ptr[j];
+				ptr[j] = ptr[j + 1];
+				ptr[j + 1] = ch;
+				chtime++;
+			}
+		}
+	}
+	return chtime;
+}
